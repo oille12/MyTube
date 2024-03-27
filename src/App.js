8760000258dd9d969573958ebfb42dc668a5f007
oille12 <oeg9176@naver.com>
2024-03-27 21:52:45 +0900

    240327 MyTube

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Search from './components/Search';
 import SideBar from './components/SideBar';
 import Survey from './components/Survey';
 
-
+// 비디오 썸네일 데이터
 const VIDEOS = [
   { id: "v0", name: "Playlist | 좋은 일은 모두 꿈에서 시작한단다, 웡카 Timothée Chalamet Wonka Playlist", src: "/images/v0-bg.jpeg", category: "music" },
   { id: "v1", name: "| Vacance Mixtape | 미뤄둔 책을 읽으며", src: "/images/v1-bg.jpeg", category: "music" },
@@ -11,6 +11,7 @@ const VIDEOS = [
   { id: "v3", name: "Playlist X ASMR 부드러운 재즈가 흐르는 숲속의 비둘기 둥지 카페 | 이런 카페 현실엔 없나요? 6th 모동숲 편", src: "/images/v3-bg.jpeg", category: "asmr" },
 ]
 
+// 필터맵 : 필터 조건 그룹 
 const FILTER_MAP = {
   전체: () => true,
   음악: ({ category }) => category === "music",
@@ -18,32 +19,36 @@ const FILTER_MAP = {
   asmr: ({ category }) => category === "asmr",
 }
 
+// 필터 이름 -> 전체, 음악, 요리, asmr
 const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 export default function App() {
-  const [filter, setFilter] = useState("전체");
-  const [sideBarActive, setSideBarActive] = useState(false);
-  const [searchActive, setSearchActive] = useState(false);
+  const [filter, setFilter] = useState("전체"); // 현재 필터
+  const [sideBarActive, setSideBarActive] = useState(false);  // 사이드바 관리 (보이지 않게 설정)
+  const [searchActive, setSearchActive] = useState(false);  // 검색창 관리 (보이지 않게 설정)
 
+  // 필터 버튼
   const filterButtons = FILTER_NAMES.map(name => (
     <button
       key={name}
       className="px-4 py-1 bg-zinc-800 text-white disabled:bg-white disabled:text-black rounded-lg"
       onClick={() => setFilter(name)}
       disabled={name === filter}
-      >
+    >
       {name}
     </button>
   ))
 
+  // 썸네일 리스트
   const videoList = VIDEOS.filter(FILTER_MAP[filter]).map(video => (
     <li key={video.id} className="mb-8">
       <img
         className="w-full"
-        // src="/images/v0-bg.jpeg"
-        src={process.env.PUBLIC_URL + video.src}
+        src={video.src}
       />
-      <h3 className="text-white font-semibold my-2">{video.name}</h3>
+      <h3 className="text-white font-semibold my-2">
+        {video.name}
+      </h3>
     </li>
   ))
 
@@ -52,7 +57,9 @@ export default function App() {
     <>
     <header className="fixed w-full top-0 box-content bg-black">
       <div className="flex justify-between h-12">
+        {/* 메뉴 로고 */}
         <div className="flex items-center">
+          {/* 사이드바 메뉴 버튼 */}
           <button
             className="px-4 h-full"
             onClick={() => setSideBarActive(true)}
@@ -65,6 +72,7 @@ export default function App() {
               <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
             </svg>
           </button>
+          {/* 유튜브 로고 */}
           <div className="flex items-center">
             <svg
               className="w-8 fill-white"
@@ -76,6 +84,7 @@ export default function App() {
             <h1 className="ml-1 text-white font-semibold">MyTube</h1>
           </div>
         </div>
+        {/* 검색 버튼 */}
         <button
           className="px-4 flex items-center"
           onClick={() => setSearchActive(true)}
@@ -90,6 +99,7 @@ export default function App() {
         </button>
       </div>
 
+      {/* 필터리스트 버튼 */}
       <div className="flex gap-2 py-2 px-4">{filterButtons}</div>
     </header>
 
@@ -100,7 +110,6 @@ export default function App() {
       <Survey />
     </main>
 
-    
     <Search
       active={searchActive}
       setActive={setSearchActive}
@@ -110,8 +119,6 @@ export default function App() {
       active={sideBarActive}
       setActive={setSideBarActive}
     />
-
-
 
     </>
   )
